543751fd08c0afff9a6848b79d8b9c7cf6deb5e2
Øyvor Salvesen Haldorsen (IT SI SIB) <ohald@be-linrgsn159.be.statoil.no>
2018-06-14 16:14:40 +0200

    bad commit

--- funtools/stringmod.py
+++ funtools/stringmod.py
@@ -1,13 +1,14 @@
 
-
 def last_first(x):
     return x[-1] + x[:len(x)-1]
 
 def mirror(x):
+    if not type(x)==str:
+        raise ValueError("Not a string object")
     word = ""
     for i in range(len(x)):
-        word =  x[i] + word
+        word =  x[i] + word + " "
     return word
 
 def psen(x):
-    return "This is your string"+x
+    return "This is your string " + x
