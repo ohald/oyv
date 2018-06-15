c5dd4ac370519ae15b2a305ec6e80374bbb6ff3f
Oyvor Salvesen Haldorsen <ohald@statoil.com>
2018-06-15 14:14:44 +0200

    Setup file added for the project

---
+++ funtools/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='stringmod',
+      version='0.0.0',
+      description='Turn word',
+      author='Oyvor Haldorsen',
+      author_email='ohald@statoil.com',
+      url='www.ohald.com',
+     )
